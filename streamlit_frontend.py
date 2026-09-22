6a875c4c179b215a99c472cd2812083fbe066395
Salmankhan0518 <salman.khan.fullstack.ai@gmail.com>
2026-09-22 15:49:33 +0500

    from here the copyCat chatbot is completed

--- streamlit_frontend.py
+++ streamlit_frontend.py
@@ -1,1 +1,27 @@
 import streamlit as st
+
+if 'message_history' not in st.session_state:
+    st.session_state['message_history'] = []
+
+
+
+# loading the converstion history 
+for message in st.session_state['message_history']:
+    with st.chat_message(message['role']):
+        st.text(message['content'])
+
+
+user_input = st.chat_input('Type here')
+
+if user_input:
+
+    # first add the message to message history
+    st.session_state['message_history'].append({'role': 'user', 'content': user_input})
+    with st.chat_message('user'):
+        st.text(user_input)
+
+    # first add the message to message history
+    st.session_state['message_history'].append({'role': 'assistant', 'content': user_input})
+    with st.chat_message('assistant'):
+        st.text(user_input)
+
