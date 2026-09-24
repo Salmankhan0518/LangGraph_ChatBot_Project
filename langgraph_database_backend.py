30e4e361b756f554d1cfb07937a44ddba16840b8
Salmankhan0518 <salman.khan.fullstack.ai@gmail.com>
2026-09-25 02:12:39 +0500

    complete chatbot

--- langgraph_database_backend.py
+++ langgraph_database_backend.py
@@ -42,13 +42,11 @@ graph.add_edge('chat_node', END)
 
 chatbot = graph.compile(checkpointer=checkpointer)
 
-# test
-CONFIG = {"configurable": {"thread_id": 'thread-1'}}
 
-response = chatbot.invoke(
-                {"messages": [HumanMessage(content='what is my name')]},
-                  config=CONFIG,
-            )
+def retrieve_all_threads():
+    all_threads = set()
+    for checkpoint in checkpointer.list(None):
+        all_threads.add(checkpoint.config['configurable']['thread_id'])
 
+    return list(all_threads)
 
-print(response)
